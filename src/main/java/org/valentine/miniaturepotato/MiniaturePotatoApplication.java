ee6e5b0aaed58192247eb6f1929d30db371e5933
Valentine0604 <ewecia.s@gmail.com>
2024-12-10 15:42:18 +0100

    Initial setup

---
+++ src/main/java/org/valentine/miniaturepotato/MiniaturePotatoApplication.java
@@ -0,0 +1,13 @@
+package org.valentine.miniaturepotato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniaturePotatoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniaturePotatoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/valentine/miniaturepotato/MiniaturePotatoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.valentine.miniaturepotato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniaturePotatoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
