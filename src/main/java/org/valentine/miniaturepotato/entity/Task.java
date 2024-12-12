18b23100530cdd134546c4b2a76aca11de9f0606
Valentine0604 <ewecia.s@gmail.com>
2024-12-12 01:38:16 +0100

    feat: add dto layer

--- src/main/java/org/valentine/miniaturepotato/entity/Task.java
+++ src/main/java/org/valentine/miniaturepotato/entity/Task.java
@@ -1,10 +1,14 @@
 package org.valentine.miniaturepotato.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
@@ -14,23 +18,31 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class Task {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
 
     @Column(nullable = false)
+    @NotNull
     private String title;
 
+    @NotNull
     private String description;
 
     @Column(name = "created_at")
     private LocalDateTime createdAt;
 
+    @NotNull
     @Column(name = "due_date")
     private LocalDateTime dueDate;
 
     @Enumerated(EnumType.STRING)
     private Priority priority;
 
-    private boolean completed;
+    private boolean isCompleted;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = LocalDateTime.now();
+    }
 
 }
